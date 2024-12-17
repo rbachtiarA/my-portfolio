e0f489eb9957705cadf19554abc42de7b7933d0c
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-12-17 15:05:40 +0700

    feat: add avatar to hero and my contact

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,14 +1,21 @@
 'use client'
+
+import Image from "next/image"
+import MyContactButton from "./myContact"
+
 export default function Hero() {
   return (
-    <div className="bg-stone-400 [&_h1]:text-2xl w-full flex justify-evenly p-8">
-        <div className="bg-slate-500 flex flex-col justify-center font-semibold p-2">
-            <h1>Hi ! I&apos;m Ryan bachtiar Abdalla</h1>
-            <h2>Full-stack Web Developer</h2>
+    <div className="[&_h1]:text-2xl w-full flex flex-col items-center justify-center md:flex-row">
+        <div className="w-[300px] md:w-[450px] flex flex-col items-center">
+          <Image src={'/avatar.png'} alt="picture of ryan" width={0} height={0} sizes="100vw" className="w-[250px] md:w-[400px]" draggable={false}/>
+          <div className="rounded-full text-center justify-center logo-dark bg-slate-100 w-full">
+            <p className="font-bold">Ryan Bachtiar Abdalla</p>
+            <p>Fullstack Web Developer</p>
+          </div>
         </div>
-
-        <div className="w-[800px] h-[200px] bg-black">
-            
+        <div className="md:w-1/2">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut ipsa officiis eveniet nam nobis non corrupti veniam magni! Tempore earum cupiditate hic officia, veritatis quod aliquam natus ab maiores! Nisi, reiciendis ipsum? Enim voluptate deserunt, voluptates voluptas architecto nobis hic, veniam cum, impedit ipsa fugiat? Iste ut amet sed necessitatibus!</p>
+          <MyContactButton />
         </div>
     </div>
   )
---
+++ src/components/hero/myContact.tsx
@@ -0,0 +1,27 @@
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+import { CiMail } from "react-icons/ci";
+import { FaGithub, FaWhatsapp } from "react-icons/fa";
+
+export default function MyContactButton() {
+    function ContactButton({children, link}: {children: ReactNode, link:string}) {
+        return (
+            <button className='bg-slate-200 rounded-full p-2 hover:bg-slate-400' onClick={() => redirect(link)}>
+                {children}
+            </button>
+        )
+    } 
+  return (
+    <div className="flex gap-2">
+        <ContactButton link="https://wa.me/+628561232435">
+            <FaWhatsapp size={40}/>
+        </ContactButton>
+        <ContactButton link="mailto:rbachtiarabdalla@gmail.com">
+            <CiMail size={40}/>
+        </ContactButton>
+        <ContactButton link="https://github.com/rbachtiarA">
+            <FaGithub size={40}/>
+        </ContactButton>
+    </div>
+  )
+}
