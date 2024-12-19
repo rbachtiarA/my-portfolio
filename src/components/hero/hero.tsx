c391e306c123874252657377bda4bdc42459ee7b
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-12-19 12:23:20 +0700

    feat: add priority

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div className="[&_h1]:text-2xl w-full flex flex-col items-center justify-center md:flex-row">
         <div className="w-[300px] md:w-[450px] flex flex-col items-center">
-          <Image src={'/avatar.png'} alt="picture of ryan" width={0} height={0} sizes="100vw" className="w-[250px] md:w-[400px]" draggable={false}/>
+          <Image priority src={'/avatar.png'} alt="picture of ryan" width={0} height={0} sizes="100vw" className="w-[250px] md:w-[400px]" draggable={false}/>
           <div className="rounded-full text-center justify-center logo-dark bg-slate-100 w-full">
             <p className="font-bold">Ryan Bachtiar Abdalla</p>
             <p>Fullstack Web Developer</p>
